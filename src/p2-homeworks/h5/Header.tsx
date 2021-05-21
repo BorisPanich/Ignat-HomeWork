53a4f17fb8c38e955c0cba0837a6cbe529eb4296
BorisPanich <panich2303@gmail.com>
2021-05-21 06:55:19 +0300

    HW5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import s from './Header.module.sass';
+import {PATH} from './Routes';
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.nav}>
+            <button className={s.menuHover}>Menu</button>
+            <div className={s.items}>
+                <div className={s.item}>
+                    <NavLink to={PATH.PRE_JUNIOR} activeClassName={s.activeLink}>PreJunior</NavLink>
+                </div>
+                <div className={s.item}>
+                    <NavLink to={PATH.JUNIOR} activeClassName={s.activeLink}>Junior</NavLink>
+                </div>
+                <div className={s.item}>
+                    <NavLink to={PATH.JUNIOR_PLUS} activeClassName={s.activeLink}>JuniorPlus</NavLink>
+                </div>
+            </div>
         </div>
     )
 }
--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import s from './Error404.module.sass';
+// import Tilt from 'react-tilt';
 
 function Error404() {
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div className={s.pageError}>
+            {/*<Tilt className={s.tilt} options={{max: 20}}>*/}
+                <div className={s.tiltInner}>
+                    <img src='https://image.freepik.com/free-vector/glitch-error-404-page_23-2148105404.jpg' alt='404'/>
+                </div>
+            {/*</Tilt>*/}
         </div>
     )
 }
